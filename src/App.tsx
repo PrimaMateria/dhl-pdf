78bdc448ea7b721151f2256d754d4d2e01aae19e
matus.benko <matus.benko@gmail.com>
2024-11-10 21:40:13 +0100

    white rectangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { ChangeEvent, useCallback, useState } from "react";
 import "./App.css";
-import { degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib";
+import { PDFDocument, rgb } from "pdf-lib";
 
 async function modifyPdf(pdfBytes: ArrayBuffer) {
   const pdfDoc = await PDFDocument.load(pdfBytes);
-  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
   const pages = pdfDoc.getPages();
   const firstPage = pages[0];
   const { width, height } = firstPage.getSize();
-  firstPage.drawText("This text was added with JavaScript!", {
-    x: 5,
-    y: height / 2 + 300,
-    size: 50,
-    font: helveticaFont,
-    color: rgb(0.95, 0.1, 0.1),
-    rotate: degrees(-45),
-  });
+
+  const svgPath = `M 0,${height / 2} L ${width},${height / 2} L ${width},${height} L 0,${height} Z`;
+
+  firstPage.moveTo(0, firstPage.getHeight());
+  firstPage.drawSvgPath(svgPath, { color: rgb(1, 1, 1) });
 
   return pdfDoc;
 }
@@ -50,7 +46,7 @@ function App() {
         <h1>DHL PDF Cleaner</h1>
 
         <div className="formField">
-          <label htmlFor="file">Choose pdf file to upload</label>
+          <label htmlFor="file">Choose PDF file to upload</label>
           <input
             type="file"
             id="file"
